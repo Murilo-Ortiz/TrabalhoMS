8e2fe98982d3ca59edd9e7014611c7cbeb8e1615
Murilo-Ortiz <91889729+Murilo-Ortiz@users.noreply.github.com>
2023-07-08 01:11:48 -0300

    Update DAOInstrutor.java

--- src/boundaries/DAOInstrutor.java
+++ src/boundaries/DAOInstrutor.java
@@ -1,4 +1,22 @@
 package boundaries;
 
 public class DAOInstrutor {
+
+  public static void cadastrarInstrutor(Instrutor instrutor){
+    Sgbd.gravar(instrutor);
+  }
+
+  public static int existeNome(String nome){
+    return Sgbd.existeAluno(nome);
+  }
+
+  public static void removeInstrutor(Instrutor instrutor){
+    Sgbd.remover(instrutor);
+  }
+
+  public static ArrayList<Instrutor> getList(){
+    ArrayList<Instrutor> lista;
+    lista = Sgbd.getInstrutoresList();
+    return lista;
+  }
 }
